2ea349e42b02137ec833baa4c493c99089c6d814
EricCR85 <ecr.ceo@gmail.com>
2025-10-29 14:21:29 -0400

    help with burger menu

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ function closeMenu(){
     document.body.classList.remove('menu--open')
 
 }
+const burgerMenu = document.getElementById('burgerMenu');
+const navMenu = document.getElementById('navMenu');
+
+burgerMenu.addEventListener('click', ()=>{
+    navMenu.style.display = navMenu.style.display === 'black'? 'none': 'block';
+})
